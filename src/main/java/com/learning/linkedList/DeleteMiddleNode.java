9ecdf01e22fd9a2ac042adbdeb3de0e015bc5d5a
toannpt <toannpt@inter-k.com>
2024-05-12 23:25:20 +0700

    LeetCode: day 1

---
+++ src/main/java/com/learning/linkedList/DeleteMiddleNode.java
@@ -0,0 +1,32 @@
+package com.learning.linkedList;
+
+import com.learning.ListNode;
+
+public class DeleteMiddleNode {
+    public static ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (head.next.next == null) {
+            head.next = null;
+            return head;
+        }
+        ListNode fast = head.next.next;
+        ListNode slow = head;
+        while (fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, null));
+        ListNode a = deleteMiddle(head);
+        while (a != null) {
+            System.out.println(a.val);
+            a = a.next;
+        }
+    }
+}
