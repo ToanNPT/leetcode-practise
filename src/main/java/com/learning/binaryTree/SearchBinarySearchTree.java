39bb119cad7838fdc410388b533730c7efa5d0ba
toannpt <toannpt0810@gmail.com>
2024-05-24 00:25:41 +0700

    leetcode: day 7

---
+++ src/main/java/com/learning/binaryTree/SearchBinarySearchTree.java
@@ -0,0 +1,33 @@
+package com.learning.binaryTree;
+
+import com.learning.TreeNode;
+
+public class SearchBinarySearchTree {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        System.out.println(new SearchBinarySearchTree().searchBST(root, 2));
+    }
+
+}
